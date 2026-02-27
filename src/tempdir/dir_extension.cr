d66ca1636013cd0e50781466b3aa0a5b2b24968d
kritoke <3081486+kritoke@users.noreply.github.com>
2026-02-26 19:52:04 -0600

    refactor(dir): use FunctionalTempdir.with_tempdir for block form of Dir.mktmpdir

--- src/tempdir/dir_extension.cr
+++ src/tempdir/dir_extension.cr
@@ -1,16 +1,13 @@
 require "./tempdir_class"
+require "./functional_tempdir"
 
 class Dir
+  # Block form: use the functional helper to guarantee cleanup
   def self.mktmpdir(**args, &blk : String -> Nil)
-    dir = Tempdir.new(**args)
-    begin
-      blk.call(dir.path)
-    ensure
-      dir.close
-    end
-    nil
+    FunctionalTempdir.with_tempdir(**args, &blk)
   end
 
+  # Non-block form remains compatible and returns a Tempdir instance
   def self.mktmpdir(**args)
     Tempdir.new(**args)
   end
