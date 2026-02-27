e4096c0eb058adff26c91f4eb72ce26d1e0022c0
kritoke <3081486+kritoke@users.noreply.github.com>
2026-02-27 04:09:56 -0600

    feat(result): add map/and_then/unwrap_or helpers to Result

--- src/tempdir/result.cr
+++ src/tempdir/result.cr
@@ -8,12 +8,12 @@ module TempdirResult
     def initialize(@ok : Bool, @value : T? = nil, @error : E? = nil)
     end
 
-    def self.ok(value : T)
-      Result.new(true, value, nil)
+    def self.ok(value : T) : Result(T, E)
+      Result(T, E).new(true, value, nil)
     end
 
-    def self.err(error : E)
-      Result.new(false, nil, error)
+    def self.err(error : E) : Result(T, E)
+      Result(T, E).new(false, nil, error)
     end
 
     def success? : Bool
@@ -26,6 +26,29 @@ module TempdirResult
 
     def error! : E
       @error.not_nil!
+    end
+
+    # Transform the success value
+    def map(&block : T -> U) : Result(U, E)
+      if @ok
+        Result(U, E).ok(block.call(@value.not_nil!))
+      else
+        Result(U, E).err(@error.not_nil!)
+      end
+    end
+
+    # Chain another Result-producing function
+    def and_then(&block : T -> Result(U, E)) : Result(U, E)
+      if @ok
+        block.call(@value.not_nil!)
+      else
+        Result(U, E).err(@error.not_nil!)
+      end
+    end
+
+    # Return the contained value or a default
+    def unwrap_or(default : T) : T
+      @ok ? @value.not_nil! : default
     end
   end
 end
